30ea7c9534f43dbee718b4973bdfd2ec05a63438
Savindu <savinduruhunuhewa@gmail.com>
2025-02-21 11:00:43 +0530

    Import the necessary libraries

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from phi.agent import Agent
+from phi.model.google import Gemini 
+from phi.tools.duckduckgo import DuckDuckGo
+from google.generativeai import upload_file,get_file
+
+
+import time
+from pathlib import Path
+
+
+import tempfile as tf
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
