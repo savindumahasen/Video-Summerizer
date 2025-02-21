568aff7875b705df05c1d0cdde4a636c4767257f
Savindu <savinduruhunuhewa@gmail.com>
2025-02-21 20:19:04 +0530

    Config the Google_API_KEY

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from phi.agent import Agent
 from phi.model.google import Gemini 
 from phi.tools.duckduckgo import DuckDuckGo
 from google.generativeai import upload_file,get_file
+import google.generativeai as genai
 
 
 import time
@@ -15,3 +16,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+if GOOGLE_API_KEY:
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+
+## Page configuration
+
+st.set_page_config(
+    page_title="Multimodel AI",
+    page_icon="",
+    layout="wide"
+)
+
+st.title("Video Summerizer AI agent")
